229022225aabb30487d1f3346aecc04420649a36
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2022-04-19 18:13:59 -0300

    Create Teste.cs

---
+++ IniciandoComOrientaçãoAObjetos/Teste.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("\nFim da Execução...");
+            Console.ReadLine();
+        }
+    }
+}
