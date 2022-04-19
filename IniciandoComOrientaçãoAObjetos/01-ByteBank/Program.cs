48971a1eb56c0f0a8cce306bb2d1759b1ddd3a89
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2022-04-19 18:17:12 -0300

    Add files via upload

---
+++ IniciandoComOrientaçãoAObjetos/01-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente(); //Uma nova instância
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo = contaDaGabriela.saldo + 150;
+
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+
+
+
+            Console.WriteLine("\nFim da execução...");
+            Console.ReadLine();
+        }
+    }
+}
